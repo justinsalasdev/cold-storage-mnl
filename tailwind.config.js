f9f517a8ff4b78b47c0497e1703d26ce24e84fe0
justinsalasdev <justinsalasdev@gmail.com>
2023-07-06 13:41:52 +0800

    logo and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        prim: {
+          50: "#D3EBFD",
+          100: "#C0E2FC",
+          200: "#94CEFA",
+          300: "#6DBCF8",
+          400: "#41A8F6",
+          500: "#1593F4",
+          600: "#0A7ED6",
+          700: "#0864AA",
+          800: "#064A7F",
+          900: "#043153",
+          950: "#032640",
+        },
       },
     },
   },
   plugins: [],
-}
+};
