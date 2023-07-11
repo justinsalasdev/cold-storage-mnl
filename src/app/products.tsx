0bb8da20bb51104b99cd925489d186cd639a1d9e
justinsalasdev <justinsalasdev@gmail.com>
2023-07-11 21:05:51 +0800

    add basic products

--- src/app/products.tsx
+++ src/app/products.tsx
@@ -6,7 +6,13 @@ export default function Products() {
       {products.map((p) => (
         <div>
           <p className="font-semibold text-lg">{p.name}</p>
-          <Image src={p.image} alt={p.name} width={400} height={200} />
+          <Image
+            src={p.image}
+            alt={p.name}
+            width={300}
+            height={200}
+            className="object-contain"
+          />
         </div>
       ))}
     </div>
@@ -26,6 +32,16 @@ export const products: TProduct[] = [
   {
     name: "Walk-in Freezer",
     price: { low: 1000, high: 2000 },
-    image: "/images/walk-in-freezer.jpg",
+    image: "/images/walk-in-freezer.jpeg",
+  },
+  {
+    name: "Blast Freezer",
+    price: { low: 1000, high: 2000 },
+    image: "/images/blast-freezer.jpeg",
+  },
+  {
+    name: "Temperature Controls",
+    price: { low: 1000, high: 2000 },
+    image: "/images/control-panels.jpeg",
   },
 ];
