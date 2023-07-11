4a066385f6c30393250c6ab3135f9f3d8d2b6441
justinsalasdev <justinsalasdev@gmail.com>
2023-07-11 20:02:07 +0800

    products section

--- src/app/products.tsx
+++ src/app/products.tsx
@@ -1,3 +1,31 @@
+import Image from "next/image";
+
 export default function Products() {
-  return <div>products</div>;
+  return (
+    <div>
+      {products.map((p) => (
+        <div>
+          <p className="font-semibold text-lg">{p.name}</p>
+          <Image src={p.image} alt={p.name} width={400} height={200} />
+        </div>
+      ))}
+    </div>
+  );
 }
+
+type TProduct = {
+  name: string;
+  price: {
+    low: number;
+    high: number;
+  };
+  image: string;
+};
+
+export const products: TProduct[] = [
+  {
+    name: "Walk-in Freezer",
+    price: { low: 1000, high: 2000 },
+    image: "/images/walk-in-freezer.jpg",
+  },
+];
