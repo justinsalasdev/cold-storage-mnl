5120a9b4fc47875699e27f9bb436edc5428bd8dd
justinsalasdev <justinsalasdev@gmail.com>
2023-07-15 18:47:27 +0800

    product cards

--- src/app/products.tsx
+++ src/app/products.tsx
@@ -4,7 +4,7 @@ export default function Products() {
   return (
     <div className="grid grid-cols-4 gap-4 mt-4">
       {products.map((p) => (
-        <div key={p.id} className="rounded border border-slate-600">
+        <div key={p.id} className="">
           <Image
             src={p.image}
             alt={p.name}
@@ -12,7 +12,10 @@ export default function Products() {
             height={200}
             className="object-cover w-full h-52"
           />
-          <p className="font-light p-2 uppercase">{p.name}</p>
+          <div className="p-2 pb-4 bg-slate-50/5">
+            <p className="font-light mt-2  uppercase">{p.name}</p>
+            <p className="mt-2 text-sm text-slate-400">product description</p>
+          </div>
         </div>
       ))}
     </div>
@@ -34,18 +37,24 @@ export const products: TProduct[] = [
     id: "1",
     name: "Walk-in Freezer",
     price: { low: 1000, high: 2000 },
-    image: "/images/walk-in-freezer.jpeg",
+    image: "/images/cold-room.jpeg",
   },
   {
     id: "2",
     name: "Blast Freezer",
     price: { low: 1000, high: 2000 },
-    image: "/images/blast-freezer.jpeg",
+    image: "/images/cold-room.jpeg",
   },
   {
     id: "3",
     name: "Temperature Controls",
     price: { low: 1000, high: 2000 },
-    image: "/images/control-panels.jpeg",
+    image: "/images/control-panel.jpeg",
+  },
+  {
+    id: "4",
+    name: "Condensers",
+    price: { low: 1000, high: 2000 },
+    image: "/images/condensers.jpeg",
   },
 ];
